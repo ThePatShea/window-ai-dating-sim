8416b1ba3974d08d953f0832e9282931d0aef571
Patrick Shea <patshea56@gmail.com>
2023-06-14 16:19:40 -0700

    implemented google analytics

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,26 @@
 import '@/styles/globals.css'
 import type { AppProps } from 'next/app'
+import Script from 'next/script'
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      <Script strategy="afterInteractive" src="https://www.googletagmanager.com/gtag/js?id=G-L7DM01KVM2"/>
+      <Script
+        id='google-analytics'
+        strategy="afterInteractive"
+        dangerouslySetInnerHTML={{
+          __html: `
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', 'G-L7DM01KVM2', {
+              page_path: window.location.pathname,
+            });
+          `,
+          }}
+      />
+      <Component {...pageProps} />
+    </>
+  )
 }
